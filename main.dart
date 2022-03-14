54ebd909fdc69464cc20e6774e75647d66db8e31
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:40:08 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_i18n/flutter_i18n.dart';
 
         listener: (context, state) => null,
         builder: (context, state) =>
