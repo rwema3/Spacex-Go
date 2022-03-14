21fa73bf9d17edd31b1c8aecd3d95c161e85960c
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:33:08 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -46,7 +46,6 @@ Future<void> main() async {
     achievementsRepository: AchievementsRepository(
       AchievementsService(httpClient),
     ),
-    companyRepository: CompanyRepository(
 
       child: BlocConsumer<ThemeCubit, ThemeState>(
         listener: (context, state) => null,
