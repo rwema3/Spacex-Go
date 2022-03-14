dd5498cb43ad61c5483b86d19f4d44dad26da1c4
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:39:54 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,8 +6,6 @@ import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:path_provider/path_provider.dart';
-
 
         listener: (context, state) => null,
         builder: (context, state) =>
