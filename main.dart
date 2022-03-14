e923415be0d2921ce026f35b5e20eee7c595a232
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:35:20 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,8 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_i18n/flutter_i18n.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
       child: BlocConsumer<ThemeCubit, ThemeState>(
         listener: (context, state) => null,
