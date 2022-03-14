5b221a53d3c6fecdaf71284ae5a961872e593fb9
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:15:01 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -15,7 +15,7 @@ import 'utils/index.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  HydratedBloc.storage = a
+  HydratedBloc.storage = 
   final httpClient = Dio();
   final notificationsCubit = kIsWeb
       ? null
