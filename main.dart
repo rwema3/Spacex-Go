72d2ac23a46cd4d1b719834d0808ca32982ec345
rwema3 <rwemadominik3@gmail.com>
2022-03-14 09:13:34 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -18,7 +18,7 @@ Future<void> main() async {
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
-        : await getTempor
+        : await getTempo
   Bloc.observer = CherryBlocObserver();
 
   final httpClient = Dio();
