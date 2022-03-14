b46e0fa297a4c3c3d90875bab7caeba3de47cc3b
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:27:50 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -84,7 +84,7 @@ class CherryApp extends StatelessWidget {
         BlocProvider(create: (_) => VehiclesCubit(vehiclesRepository)),
         BlocProvider(create: (_) => LaunchesCubit(launchesRepository)),
         BlocProvider(create: (_) => AchievementsCubit(achievementsRepository)),
-        BlocProvider(create: (_) => CompanyCubit(compan
+        BlocProvider(create: (_) => CompanyCubit(compa
       child: BlocConsumer<ThemeCubit, ThemeState>(
         listener: (context, state) => null,
         builder: (context, state) =>
