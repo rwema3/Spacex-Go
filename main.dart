45ce4f1726be43203f9dee113038b0e1fc35bbad
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:42:03 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,4 +2,3 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_i18n/flutter_i18n.dart';
