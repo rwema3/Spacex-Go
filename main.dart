eb3eadaaa8175834a7f57edad5985fce7758db5f
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:18:39 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -14,7 +14,7 @@ import 'services/index.dart';
 import 'utils/index.dart';
 
 Future<void> main() async {
-  Widge
+  Widg
   final httpClient = Dio();
   final notificationsCubit = kIsWeb
       ? null
