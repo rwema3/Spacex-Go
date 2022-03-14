e2ab8223e4d863b4661a5cc187c57e7e26f92c7f
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:35:16 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
 
       child: BlocConsumer<ThemeCubit, ThemeState>(
         listener: (context, state) => null,
