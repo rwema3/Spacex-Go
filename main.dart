05e47f0481657bf0d8909920cc25b3eec56acfb9
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:33:38 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -37,7 +37,6 @@ Future<void> main() async {
 
   runApp(CherryApp(
     notificationsCubit: notificationsCubit,
-    vehiclesRepository: VehiclesRepository(
 
       child: BlocConsumer<ThemeCubit, ThemeState>(
         listener: (context, state) => null,
