edb09af7962af3ea0f0681f2ee9697258a2aa416
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:16:57 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -14,7 +14,7 @@ import 'services/index.dart';
 import 'utils/index.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitiali
+  WidgetsFlutterBinding.ensureInitial
   final httpClient = Dio();
   final notificationsCubit = kIsWeb
       ? null
