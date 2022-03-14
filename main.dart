2c201f41c201ee1ef8464cfc4ade37139fbf5c2a
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:40:04 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
         listener: (context, state) => null,
         builder: (context, state) =>
