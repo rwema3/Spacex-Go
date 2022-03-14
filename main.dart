042ec73a1acbf7469c83de3baee30341d46b1e24
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:32:41 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -53,8 +53,6 @@ Future<void> main() async {
       ChangelogService(httpClient),
     ),
   ));
-}
-
 
       child: BlocConsumer<ThemeCubit, ThemeState>(
         listener: (context, state) => null,
