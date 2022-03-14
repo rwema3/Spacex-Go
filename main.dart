5a0da0cded11720c70dc5fa37ff9589a25f18664
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:35:27 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 
       child: BlocConsumer<ThemeCubit, ThemeState>(
         listener: (context, state) => null,
