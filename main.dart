ac8abb800793cf20abf7f6129580e04b1ae75d0e
rwema3 <rwemadominik3@gmail.com>
2022-03-14 10:43:43 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,5 +4,3 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
