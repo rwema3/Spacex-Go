fe95789121050ceffdc192905a8445c8b6a3506f
rwema3 <rwemadominik3@gmail.com>
2022-03-14 09:16:10 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -17,7 +17,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
-        ? Hydrate
+        ? Hydrat
   Bloc.observer = CherryBlocObserver();
 
   final httpClient = Dio();
